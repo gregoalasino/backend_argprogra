a69586021dfd02c9276d9433a4dd324d777e79ad
Gregorio Alasino <gregoalasino@gmail.com>
2022-11-11 11:08:46 -0300

    cambio cors en main

--- src/main/java/com/portfolio/gregorioalasino/GregorioalasinoApplication.java
+++ src/main/java/com/portfolio/gregorioalasino/GregorioalasinoApplication.java
@@ -1,13 +1,37 @@
 package com.portfolio.gregorioalasino;
 
+import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class GregorioalasinoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GregorioalasinoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GregorioalasinoApplication.class, args);
+    }
+    
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200/","https://frontendgregoalasino.web.app/"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+                "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
 
 }
+
+
+    
